e66611cc759d6ac7113a8512a02e25951731a7f3
pbmoeller <sonla@gmx.net>
2024-08-08 22:25:40 +0200

    Added documentation of the API.

--- include/cpp-anvil/io/compression.hpp
+++ include/cpp-anvil/io/compression.hpp
@@ -8,6 +8,7 @@
 namespace nbt {
 namespace io {
 
+//! @brief The supported compression types.
 enum class CompressionType
 {
     Gzip = 1,
@@ -15,25 +16,61 @@ enum class CompressionType
     Uncompressed = 3,
 };
 
+//! @brief Checks if sequence of bytes begins with Gzip header.
+//! 
+//! @param data The data to be checked.
+//! 
+//! @return `true` if the first 2 bytes in the sequence indicate a Gzip header, `false` otherwise.
 constexpr bool isGzipCompressed(const std::vector<unsigned char> &data) {
     return (data.size() >= 2) 
         && (data[0] == 0x1F && data[1] == 0x8B);
 }
 
+//! @brief Checks if sequence of bytes begins with Zlib header.
+//! 
+//! @param data The data to be checked.
+//! 
+//! @return `true` if the first 2 bytes in the sequence indicate a Zlib header, `false` otherwise.
 constexpr bool isZlibCompressed(const std::vector<unsigned char> &data) {
     return (data.size() >= 2) 
         && (data[0] == 0x78 
             && (data[1] == 0x8B || data[1] == 0x5E || data[1] == 0x9C || data[1] == 0xDA));
 }
+
+//! @brief Checks if sequence of bytes begins with neither Zlib or Gzip header.
+//! 
+//! @param data The data to be checked.
+//! 
+//! @return `true` if the first 2 bytes do not indicate Gzip or Zlib header, `false` otherwise.
 constexpr bool isUncompressed(const std::vector<unsigned char> &data) {
     return !isGzipCompressed(data) && !isZlibCompressed(data);
 }
 
+//! @brief Tests type of compression of the data sequence by looking for Gzip or Zlib header.
+//! 
+//! @param data The data to be checked.
+//! 
+//! @return CompressionType of data sequence.
 CompressionType testCompression(const std::vector<unsigned char> &data);
 
 bool uncompress(std::ifstream &strm, std::vector<unsigned char> &data);
 
+//! @brief Uncompresses gzip data stream into byte vector.
+//! 
+//! @param strm The stream to read compressed data bytes from.
+//! @param data The target container to write the uncompressed data to. The container will be 
+//!             cleared and resized to fit to the uncompressed data.
+//! 
+//! @return `true` if uncompressing succeeded, `false` otherwise.
 bool inflate_gzip(std::ifstream &strm, std::vector<unsigned char> &data);
+
+//! @brief Uncompresses zlib data stream into byte vector.
+//! 
+//! @param strm The stream to read compressed data bytes from.
+//! @param data The target container to write the uncompressed data to. The container will be 
+//!             cleared and resized to fit to the uncompressed data.
+//! 
+//! @return `true` if uncompressing succeeded, `false` otherwise.
 bool inflate_zlib(std::ifstream &strm, std::vector<unsigned char> &data);
 
 bool deflate_gzip(std::ifstream &strm, std::vector<unsigned char> &data);
--- include/cpp-anvil/io/io.hpp
+++ include/cpp-anvil/io/io.hpp
@@ -11,13 +11,48 @@
 namespace nbt {
 namespace io {
 
+//! @brief Tries to load a NBT file.
+//! 
+//! @param filename File with NBT content. May be compressed with either `Zlib` or `Gzip` 
+//!                 compression.
+//! 
+//! @return A CompoundTag containing the NBT data.
 std::unique_ptr<CompoundTag> loadFile(const std::string &filename);
+ 
+//! @brief Tries to load a NBT file and returns \p compressionType of the raw data.
+//! 
+//! @param filename         File with NBT content. May be compressed with either `Zlib` or `Gzip`
+//!                         compression.
+//! @param compressionType  On successful read the parameter contains the compression type of the
+//!                         original data.
+//! 
+//! @return A CompoundTag containing the NBT data.
 std::unique_ptr<CompoundTag> loadFile(const std::string &filename,
                                       CompressionType &compressionType);
 
+//! @brief Deserializes a sequence of bytes into a NBT CompoundTag.
+//! 
+//! The first tag in the sequence must be a CompoundTag.
+//! 
+//! @param data  Sequence of bytes to be deserialized.
+//! 
+//! @return CompoudTag with all contents.
 std::unique_ptr<CompoundTag> readData(std::vector<unsigned char> &data);
+
+//! @brief Serializes a NBT tag into a sequence of bytes.
+//! 
+//! @param tag  The tag to be serialized.
+//! 
+//! @return Serialized sequence of bytes.
 std::vector<unsigned char> writeData(const BasicTag *tag);
 
+//! @brief Prints the \p tag and its contents.
+//! 
+//! @param tag                  The to be printed.
+//! @param indent               Indentation for inner tags.
+//! @param printArrayContent    If the contents of arrays should be printed.
+//! 
+//! @return The printed NBT tags as string.
 std::string printTag(const BasicTag *tag, int indent = 2, bool printArrayContent = false);
 
 } // namespace io
