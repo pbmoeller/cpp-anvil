be4868dafe8f8fd8cd87278fcfb7619361542859
pbmoeller <sonla@gmx.net>
2024-08-13 20:14:17 +0200

    Added version header to library.

---
+++ include/cpp-anvil/version.hpp
@@ -0,0 +1,14 @@
+#ifndef CPP_ANVIL_VERSION_HPP
+#define CPP_ANVIL_VERSION_HPP
+
+namespace nbt {
+
+unsigned int getCppAnvilVersionMajor();
+unsigned int getCppAnvilVersionMinor();
+unsigned int getCppAnvilVersionPatch();
+
+const char * getCppAnvilVersion();
+
+} // namespace nbt
+
+#endif // CPP_ANVIL_VERSION_HPP
