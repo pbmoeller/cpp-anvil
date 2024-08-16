8ca3c967fb624b212816e4926984dfcb73859d1f
pbmoeller <sonla@gmx.net>
2024-08-16 10:43:14 +0200

    Added test for loading from file.

---
+++ test/nbt/test_io.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <cpp-anvil/nbt/io.hpp>
+
+#include "test_data.hpp"
+
+TEST(io, load_file_gzip)
+{
+    const std::string testFolderNbt = "../../../test/data/";
+
+    anvil::CompressionType type;
+    auto ctag = anvil::loadFromFile(testFolderNbt + "bigtest.nbt", type);
+    ASSERT_TRUE(ctag != nullptr);
+    EXPECT_EQ(type, anvil::CompressionType::Gzip);
+
+    std::vector<unsigned char> data = anvil::writeData(ctag.get());
+
+    EXPECT_THAT(std::vector<unsigned char>(data.begin(),
+                                           data.begin() + data.size()),
+                ::testing::ElementsAreArray(&bigtestUncompressedData[0],
+                                            &bigtestUncompressedData[0] + 1544));
+}
