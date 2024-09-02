1738fc3d619f097f25a0a4e5a32fcf0503995080
pbmoeller <sonla@gmx.net>
2024-09-02 22:26:43 +0200

    Created new common header for anvil files.

---
+++ include/cpp-anvil/anvil.hpp
@@ -0,0 +1,9 @@
+#ifndef CPP_ANVIL_ANVIL_HPP
+#define CPP_ANVIL_ANVIL_HPP
+
+// cpp-anvil
+#include <cpp-anvil/anvil/chunk.hpp>
+#include <cpp-anvil/anvil/coordinates.hpp>
+#include <cpp-anvil/anvil/region.hpp>
+
+#endif // CPP_ANVIL_ANVIL_HPP
