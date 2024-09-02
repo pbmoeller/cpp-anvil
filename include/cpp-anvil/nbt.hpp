6ae43d5c94a2a7027e7dca48eae9bf3bb9effbde
pbmoeller <sonla@gmx.net>
2024-09-02 21:12:20 +0200

    Removed dead code.

--- include/cpp-anvil/nbt.hpp
+++ include/cpp-anvil/nbt.hpp
@@ -10,42 +10,4 @@
 #include <cpp-anvil/nbt/types.hpp>
 #include <cpp-anvil/nbt/io.hpp>
 
-// template<typename T, TagType TAG>
-// class ArrayTag : public NamedTag
-// {
-
-
-// //
-// //    // Capacity
-// //    constexpr size_type maxSize() const noexcept { m_value.max_size(); }
-// //    void reserve(size_type newCapacity) { m_value.reserve(newCapacity); }
-// //
-// //    // Modifiers
-// //    template<class...Args>
-// //    iterator emplace(const_iterator pos, Args&&... args) { m_value.emplace(pos, std::forward<Args>(args)...); }
-// //    template<class...Args>
-// //    reference emplace_back(Args&&... args) { m_value.emplace_back(std::forward<Args>(args)...); }
-// //    constexpr iterator erase(const_iterator pos) { m_value.erase(pos); }
-// //    constexpr iterator erase(const_iterator first, const_iterator last) { m_value.erase(first, last); }
-// //    constexpr void eraseAt(size_type pos) { m_value.erase(m_value.begin() + pos); }
-// //
-// //    constexpr iterator insert(const_iterator pos, const T& value) { return m_value.insert(pos, value); }
-// //    constexpr iterator insert(const_iterator pos, T&& value) { return m_value.insert(pos, std::move(value)); }
-// //    constexpr iterator insert(const_iterator pos, size_type count, const T& value) { 
-// //        return m_value.insert(pos, count, value);
-// //    }
-// //    template<class InputIt>
-// //    constexpr iterator insert(const_iterator pos, InputIt first, InputIt last) {
-// //        return m_value.insert(pos, first, last);
-// //    }
-// //    constexpr iterator insert(const_iterator pos, std::initializer_list<T> ilist) {
-// //        return m_value.insert(pos, ilist);
-// //    }
-// //
-// //    constexpr void resize(size_type count) { m_value.resize(count); }
-// //    constexpr void resize(size_type count, const value_type& value) { m_value.resize(count, value); }
-// 
-
-// };
-
 #endif // CPP_NBT_NBT_HPP
